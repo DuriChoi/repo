93d7230f881510f8b386aedee9aa5423dfe781cb
drchoi <skyholer@gmail.com>
2021-09-05 19:43:09 +0900

    210905 incheon

---
+++ study/divide/divide.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a=30;
+
+	int b[32];
+	printf("%ld\n",sizeof(b));
+
+	printf("%d\n",a%16);
+	printf("%d\n",a&14);
+	
+	int c, d;
+	c==1? d=10: c==2?d=20:c==3?d=30:c=3;
+	
+	return 0;
+}
